93e79cfebf3f319f0460a86e889611c3110bdea5
Lee <61103427+Cirex04@users.noreply.github.com>
2023-03-23 06:28:41 +0800

    Added the Post node for the linked list (ArrayList) data structure
    Provided depth and documentation for the Post node and it's properties, developed the csv database for post nodes and their Unique IDs (UIDs)

---
+++ BookFace/src/main/java/com/project/bookface/Post.java
@@ -0,0 +1,27 @@
+package com.project.bookface;
+
+import java.util.Date;
+
+/**
+ * @brief Post model (node) - detail of the Post component
+ */
+
+public class Post {
+    /**
+     * properties
+     */
+    private String text;
+    private Date TimeStamp;
+    private int likes;
+    private String UID;
+
+    /**
+     * constructors
+     */
+    public Post() {
+    }
+
+    /**
+     * behaviors
+     */
+}
